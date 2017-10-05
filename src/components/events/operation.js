8a233a1cd6ea484a4799be3330a4bb0295cac135
Ted Werbel <werbel.ted@gmail.com>
2017-10-05 16:45:52 -0400

    started redesigning operation/training screens - WIP

--- src/components/events/operation.js
+++ src/components/events/operation.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, ScrollView, TouchableOpacity } from 'react-native';
+import { TabNavigator } from 'react-navigation';
 import { BlurView } from 'expo';
+import { Ionicons } from '@expo/vector-icons';
 
 class Operation extends Component {
   static navigationOptions = {
@@ -9,59 +11,150 @@ class Operation extends Component {
   }
 
   render() {
+    const { goBack } = this.props.navigation;
+
     return (
-      <View>
-        <Image style={styles.cardImage} source={require('../../../assets/images/forest.png')}>
-          <BlurView tint={'dark'} intensity={80} style={{ height: '100%', width: '100%', left: 0, position: 'relative' }}>
-            <View style={styles.eventTypeContainer}>
-              <View style={styles.line} />
-              <Text style={styles.eventType}>OPERATION</Text>
-              <View style={styles.line} />
-            </View>
-            <Text style={styles.name}>OPEN ROAD</Text>
-            <Text style={styles.day}>03</Text>
-            <Text style={styles.month}>SEPTEMBER</Text>
-            <Text style={styles.time}>15:30 / 20:30</Text>
+      <View style={{ flex: 1, backgroundColor: 'black' }}>
+        <View style={{ height: 22, width: '100%', position: 'relative', backgroundColor: '#e0e0e0' }}>
+          <Text style={styles.day}>03</Text>
+          <Text style={styles.month}>SEPTEMBER</Text>
+          <Text style={styles.time}>15:30 / 20:30</Text>
+        </View>
+        <ScrollView style={styles.container}>
+          <View style={styles.imageBackground} />
+          <Image style={styles.cardImage} source={require('../../../assets/images/forest.png')}>
+            <BlurView tint={'dark'} intensity={80} style={{ height: '100%', width: '100%', left: 0, position: 'relative' }}>
+              <View style={styles.eventTypeContainer}>
+                <View style={styles.line} />
+                <Text style={styles.eventType}>OPERATION</Text>
+                <View style={styles.line} />
+              </View>
+              <Text style={styles.name}>OPEN ROAD</Text>
+            </BlurView>
+          </Image>
+          <View style={styles.sectionBar}>
+            <TouchableOpacity style={styles.sectionButton}><Text style={styles.sectionTitle}>US FORCES</Text></TouchableOpacity>
+            <Text style={styles.sectionDivider}>|</Text>
+            <TouchableOpacity style={styles.sectionButton}><Text style={[styles.sectionTitle, styles.selectedTitle]}>INS FORCES</Text></TouchableOpacity>
+            <Text style={styles.sectionDivider}>|</Text>
+            <TouchableOpacity style={styles.sectionButton}><Text style={styles.sectionTitle}>SESSION I</Text></TouchableOpacity>
+            <Text style={styles.sectionDivider}>|</Text>
+            <TouchableOpacity style={styles.sectionButton}><Text style={styles.sectionTitle}>SESSION II</Text></TouchableOpacity>
+          </View>
+          <View style={styles.divider} />
+        </ScrollView>
+        <TouchableOpacity activeOpacity={0.6} onPress={() => goBack()}>
+          <BlurView style={styles.backButton} intensity={80}>
+            <Ionicons name="ios-arrow-round-back" color="#252525" size={35} />
           </BlurView>
-        </Image>
+        </TouchableOpacity>
       </View>
     );
   }
 }
 
 const styles = {
+  container: {
+    backgroundColor: '#e0e0e0',
+    flex: 1,
+  },
+  imageBackground: {
+    backgroundColor: '#e0e0e0',
+    position: 'absolute',
+    top: 85,
+    height: 100,
+    width: '100%'
+  },
+  divider: {
+    height: 1,
+    backgroundColor: '#bdbdbd',
+    width: '100%'
+  },
+  sectionBar: {
+    flex: 1,
+    width: '100%',
+    alignSelf: 'center',
+    flexDirection: 'row',
+    paddingLeft: 15,
+    paddingRight: 15,
+    justifyContent: 'space-between',
+    backgroundColor: '#e0e0e0',
+    borderRadius: 5
+  },
+  sectionButton: {
+    paddingTop: 12,
+    paddingBottom: 8
+  },
+  backButton: {
+    position: 'absolute',
+    bottom: 27.5,
+    right: 25,
+    borderRadius: 32.5,
+    height: 65,
+    width: 65,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    opacity: 0.9,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.75,
+    shadowRadius: 6,
+    elevation: 1,
+  },
+  sectionTitle: {
+    fontFamily: 'rubik-light',
+    color: '#000000',
+    letterSpacing: 2.25,
+    fontSize: 10,
+    backgroundColor: 'transparent',
+    opacity: 0.75,
+    alignSelf: 'center'
+  },
+  sectionDivider: {
+    fontFamily: 'rubik-light',
+    color: '#000000',
+    letterSpacing: 2.25,
+    fontSize: 10,
+    backgroundColor: 'transparent',
+    opacity: 0.5,
+    alignSelf: 'center'
+  },
+  selectedTitle: {
+    opacity: 1
+  },
   day: {
     fontFamily: 'rubik-light',
-    color: '#eee',
+    color: '#252525',
     letterSpacing: 2.25,
     fontSize: 9.5,
     backgroundColor: 'transparent',
-    opacity: 0.55,
+    opacity: 0.8,
     position: 'absolute',
-    top: 11,
-    left: 10
+    top: 6,
+    left: 8
   },
   month: {
     fontFamily: 'syncopate',
-    color: '#eee',
+    color: '#252525',
     letterSpacing: 2.25,
     fontSize: 9.5,
     backgroundColor: 'transparent',
-    opacity: 0.55,
+    opacity: 0.8,
     position: 'absolute',
-    top: 12.3,
-    left: 32.5
+    top: 7.3,
+    left: 30.5
   },
   time: {
     fontFamily: 'rubik-light',
-    color: '#eee',
+    color: '#252525',
     letterSpacing: 2,
     fontSize: 9.5,
     backgroundColor: 'transparent',
-    opacity: 0.55,
+    opacity: 0.8,
     position: 'absolute',
-    top: 11,
-    right: 10
+    top: 6,
+    right: 8
   },
   eventTypeContainer: {
     flex: 1,
@@ -87,7 +180,7 @@ const styles = {
     marginLeft: 17.5,
     marginRight: 17.5,
     backgroundColor: 'transparent',
-    opacity: 0.75
+    opacity: 0.7
   },
   name: {
     fontFamily: 'syncopate',
@@ -97,14 +190,17 @@ const styles = {
     alignSelf: 'center',
     marginTop: 20,
     backgroundColor: 'transparent',
-    opacity: 0.95,
+    opacity: 0.9,
     position: 'absolute',
-    bottom: 60
+    bottom: 65
   },
   cardImage: {
     height: 175,
-    width: '100%',
-    position: 'relative'
+    width: '97.5%',
+    position: 'relative',
+    alignSelf: 'center',
+    borderRadius: 5,
+    marginTop: 5
   },
 };
 
--- src/components/events/training.js
+++ src/components/events/training.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, ScrollView, TouchableOpacity } from 'react-native';
+import { BlurView, LinearGradient } from 'expo';
+import { SimpleLineIcons } from '@expo/vector-icons';
 
 class Training extends Component {
   static navigationOptions = {
@@ -8,12 +10,174 @@ class Training extends Component {
   }
 
   render() {
+    const { goBack } = this.props.navigation;
+    const image = require('../../../assets/images/forest.png');
+
     return (
-      <View>
-        <Text>Training</Text>
+      <View style={{ flex: 1 }}>
+
+        <Image style={styles.backgroundImage} source={image}>
+          <BlurView intensity={75} style={{ position: 'absolute', top: 0, height: '100%', width: '100%' }} />
+        </Image>
+
+        <TouchableOpacity activeOpacity={0.9} style={styles.floatingCard}>
+          <Image style={styles.cardImage} source={image || require('../../../assets/images/default.png')}>
+            <BlurView intensity={90} style={{ height: '100%', width: '67.5%', left: 0, position: 'relative' }}>
+              <Text style={styles.cardDay}>03</Text>
+              <Text style={styles.cardMonth}>SEPTEMBER</Text>
+            </BlurView>
+          </Image>
+          <TouchableOpacity activeOpacity={0.75} style={styles.outerCircle}>
+            <View style={styles.innerCircle}>
+              <SimpleLineIcons style={styles.playIcon} name="game-controller" size={20} color="#4922e3" />
+            </View>
+          </TouchableOpacity>
+        </TouchableOpacity>
+
+        <View style={styles.infoContainer}>
+          <View style={styles.sectionBar}>
+            <TouchableOpacity style={styles.sectionButton}><Text style={styles.sectionTitle}>US FORCES</Text></TouchableOpacity>
+            <Text style={styles.sectionDivider}>|</Text>
+            <TouchableOpacity style={styles.sectionButton}><Text style={[styles.sectionTitle, styles.selectedTitle]}>INS FORCES</Text></TouchableOpacity>
+            <Text style={styles.sectionDivider}>|</Text>
+            <TouchableOpacity style={styles.sectionButton}><Text style={styles.sectionTitle}>SESSION I</Text></TouchableOpacity>
+            <Text style={styles.sectionDivider}>|</Text>
+            <TouchableOpacity style={styles.sectionButton}><Text style={styles.sectionTitle}>SESSION II</Text></TouchableOpacity>
+          </View>
+        </View>
+
       </View>
     );
   }
 }
+
+const styles = {
+  backgroundImage: {
+    height: 180,
+    width: '100%',
+    position: 'relative'
+  },
+  cardImage: {
+    height: 110,
+    width: '100%',
+    borderRadius: 12,
+    position: 'relative'
+  },
+  outerCircle: {
+    position: 'absolute',
+    bottom: 11,
+    right: 77.5,
+    height: 85,
+    width: 85,
+    borderRadius: 50,
+    borderWidth: 1.5,
+    borderColor: "rgba(245, 245, 245, 0.25)",
+    backgroundColor: 'transparent',
+    justifyContent: 'center',
+    flex: 1
+  },
+  innerCircle: {
+    backgroundColor: '#fff',
+    height: 60,
+    width: 60,
+    borderRadius: 100,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.15,
+    shadowRadius: 15,
+    elevation: 1,
+    opacity: 0.95
+  },
+  playIcon: {
+    backgroundColor: 'transparent',
+    alignSelf: 'center',
+    left: 1
+  },
+  checkIcon: {
+    backgroundColor: 'transparent',
+    alignSelf: 'center'
+  },
+  cardDay: {
+    fontFamily: 'rubik',
+    fontSize: 41,
+    color: '#fff',
+    letterSpacing: 0.15,
+    backgroundColor: 'transparent',
+    position: 'absolute',
+    bottom: 35,
+    paddingLeft: 24,
+    paddingRight: 25,
+    textShadowColor: 'rgba(0, 0, 0, 0.35)',
+    textShadowOffset: { width: 1, height: 1 },
+    textShadowRadius: 15,
+    elevation: 1,
+  },
+  cardMonth: {
+    fontFamily: 'syncopate',
+    fontSize: 15,
+    color: '#fff',
+    letterSpacing: 1.25,
+    backgroundColor: 'transparent',
+    position: 'absolute',
+    bottom: 15,
+    left: 25,
+  },
+  floatingCard: {
+    position: 'absolute',
+    top: 105,
+    height: 110,
+    width: '90%',
+    right: 15,
+    backgroundColor: '#eee',
+    zIndex: 5,
+    borderRadius: 12,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.5,
+    shadowRadius: 13,
+    elevation: 1
+  },
+  infoContainer: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: '#f5f5f5',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: -1 },
+    shadowOpacity: 0.5,
+    shadowRadius: 15,
+    elevation: 1
+  },
+  sectionBar: {
+    flex: 1,
+    justifyContent: 'space-between',
+    width: '100%',
+    alignSelf: 'center',
+    flexDirection: 'row',
+    paddingLeft: 15,
+    paddingRight: 15,
+  },
+  sectionButton: {
+    paddingTop: 90
+  },
+  sectionTitle: {
+    fontFamily: 'rubik-light',
+    color: '#000000',
+    letterSpacing: 2.25,
+    fontSize: 10,
+    backgroundColor: 'transparent',
+    opacity: 0.75,
+  },
+  sectionDivider: {
+    fontFamily: 'rubik-light',
+    color: '#000000',
+    letterSpacing: 2.25,
+    fontSize: 10,
+    backgroundColor: 'transparent',
+    opacity: 0.4,
+    paddingTop: 90
+  },
+};
 
 export { Training };
