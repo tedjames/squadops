bb4145a42c8d061a3705e9f6f109cc61cf22fde5
Ted Werbel <werbel.ted@gmail.com>
2017-12-26 17:43:32 -0500

    converted eventCard, profile, + serverCard to pure functional components

--- src/components/home/eventCard.js
+++ src/components/home/eventCard.js
@@ -1,127 +1,126 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { TouchableOpacity, Text, ImageBackground, View, Dimensions } from 'react-native';
 import { LinearGradient, BlurView } from 'expo';
 import { Entypo, MaterialCommunityIcons } from '@expo/vector-icons';
 
-export default class EventCard extends Component {
-  render() {
-    const { name, day, month, registered, intensity, image, gradient, tint, navigate, type } = this.props;
-    const screenWidth = Dimensions.get('window').width;
-    const cardWidth = screenWidth / 1.25;
-    const cardHeight = screenWidth / 2.5;
+const screenWidth = Dimensions.get('window').width;
+const cardWidth = screenWidth / 1.25;
+const cardHeight = screenWidth / 2.5;
 
-    const styles = {
-      outerCircle: {
-        position: 'absolute',
-        bottom: 20,
-        right: 77.5,
-        height: 85,
-        width: 85,
-        borderRadius: 50,
-        borderWidth: 1.5,
-        borderColor: "rgba(245, 245, 245, 0.25)",
-        backgroundColor: 'transparent',
-        justifyContent: 'center',
-        flex: 1
-      },
-      innerCircle: {
-        backgroundColor: '#fff',
-        height: 60,
-        width: 60,
-        borderRadius: 100,
-        alignSelf: 'center',
-        justifyContent: 'center',
-        opacity: 0.925
-      },
-      playIcon: {
-        backgroundColor: 'transparent',
-        alignSelf: 'center',
-        left: 1
-      },
-      checkIcon: {
-        backgroundColor: 'transparent',
-        alignSelf: 'center'
-      },
-      card: {
-        height: cardHeight,
-        width: cardWidth,
-        borderRadius: 12,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 1 },
-        shadowOpacity: 0.35,
-        shadowRadius: 8,
-        elevation: 2,
-        marginBottom: 15,
-        marginTop: 15,
-        marginLeft: 20
-      },
-      cardImage: {
-        height: cardHeight,
-        width: cardWidth,
-        borderRadius: 12,
-        position: 'relative'
-      },
-      cardName: {
-        fontFamily: 'rubik',
-        fontSize: 14,
-        color: '#fff',
-        letterSpacing: 0.35,
-        backgroundColor: 'transparent',
-        position: 'absolute',
-        top: 20,
-        left: 25
-      },
-      cardDay: {
-        fontFamily: 'rubik',
-        fontSize: 41,
-        color: '#fff',
-        letterSpacing: 0.15,
-        backgroundColor: 'transparent',
-        position: 'absolute',
-        bottom: 35,
-        paddingLeft: 24,
-        paddingRight: 25,
-        textShadowColor: 'rgba(0, 0, 0, 0.075)',
-        textShadowOffset: { width: 2, height: 2 },
-        textShadowRadius: 10,
-        elevation: 1,
-      },
-      cardMonth: {
-        fontFamily: 'syncopate',
-        fontSize: 15,
-        color: '#fff',
-        letterSpacing: 1,
-        backgroundColor: 'transparent',
-        position: 'absolute',
-        bottom: 15,
-        left: 25,
-      },
-    };
+const EventCard = ({ name, day, month, registered, intensity, image, gradient, tint, navigate, type }) => {
+  const styles = {
+    outerCircle: {
+      position: 'absolute',
+      bottom: 20,
+      right: 77.5,
+      height: 85,
+      width: 85,
+      borderRadius: 50,
+      borderWidth: 1.5,
+      borderColor: "rgba(245, 245, 245, 0.25)",
+      backgroundColor: 'transparent',
+      justifyContent: 'center',
+      flex: 1
+    },
+    innerCircle: {
+      backgroundColor: '#fff',
+      height: 60,
+      width: 60,
+      borderRadius: 100,
+      alignSelf: 'center',
+      justifyContent: 'center',
+      opacity: 0.925
+    },
+    playIcon: {
+      backgroundColor: 'transparent',
+      alignSelf: 'center',
+      left: 1
+    },
+    checkIcon: {
+      backgroundColor: 'transparent',
+      alignSelf: 'center'
+    },
+    card: {
+      height: cardHeight,
+      width: cardWidth,
+      borderRadius: 12,
+      shadowColor: '#000',
+      shadowOffset: { width: 0, height: 1 },
+      shadowOpacity: 0.35,
+      shadowRadius: 8,
+      elevation: 2,
+      marginBottom: 15,
+      marginTop: 15,
+      marginLeft: 20
+    },
+    cardImage: {
+      height: cardHeight,
+      width: cardWidth,
+      borderRadius: 12,
+      position: 'relative'
+    },
+    cardName: {
+      fontFamily: 'rubik',
+      fontSize: 14,
+      color: '#fff',
+      letterSpacing: 0.35,
+      backgroundColor: 'transparent',
+      position: 'absolute',
+      top: 20,
+      left: 25
+    },
+    cardDay: {
+      fontFamily: 'rubik',
+      fontSize: 41,
+      color: '#fff',
+      letterSpacing: 0.15,
+      backgroundColor: 'transparent',
+      position: 'absolute',
+      bottom: 35,
+      paddingLeft: 24,
+      paddingRight: 25,
+      textShadowColor: 'rgba(0, 0, 0, 0.075)',
+      textShadowOffset: { width: 2, height: 2 },
+      textShadowRadius: 10,
+      elevation: 1,
+    },
+    cardMonth: {
+      fontFamily: 'syncopate',
+      fontSize: 15,
+      color: '#fff',
+      letterSpacing: 1,
+      backgroundColor: 'transparent',
+      position: 'absolute',
+      bottom: 15,
+      left: 25,
+    },
+  };
 
-    return (
-      <TouchableOpacity activeOpacity={0.625} style={styles.card} onPress={() => navigate(type)}>
-        <ImageBackground style={styles.cardImage} source={image || require('../../../assets/images/default.png')} borderRadius={12}>
-        { gradient ?
-          <LinearGradient style={{ opacity: 0.9, height: '100%', width: '65%', left: 0, position: 'relative' }} colors={gradient} start={{x: 0.0, y: 0.25}} end={{x: 0.5, y: 1.0}}>
-            <Text style={styles.cardName}>{name}</Text>
-            <Text style={styles.cardDay}>{day}</Text>
-            <Text style={styles.cardMonth}>{month}</Text>
-          </LinearGradient> :
-          <BlurView tint={tint || 'default'} intensity={intensity || 85} style={{ height: '100%', width: '65%', left: 0, position: 'relative', borderBottomLeftRadius: 12, borderTopLeftRadius: 12 }}>
-            <Text style={styles.cardName}>{name}</Text>
-            <Text style={styles.cardDay}>{day}</Text>
-            <Text style={styles.cardMonth}>{month}</Text>
-          </BlurView>
-        }
-          <View style={styles.outerCircle}>
-            <View style={styles.innerCircle}>
-              { registered ?
-                <MaterialCommunityIcons style={styles.checkIcon} name="check-all" size={20} color="#512de0" /> :
-                <Entypo style={styles.playIcon} name="controller-play" size={20} color="#4922e3" /> }
-            </View>
+  return (
+    <TouchableOpacity activeOpacity={0.625} style={styles.card} onPress={() => navigate(type)}>
+      <ImageBackground style={styles.cardImage} source={image || require('../../../assets/images/default.png')} borderRadius={12}>
+      { gradient ?
+        <LinearGradient style={{ opacity: 0.9, height: '100%', width: '65%', left: 0, position: 'relative' }} colors={gradient} start={{x: 0.0, y: 0.25}} end={{x: 0.5, y: 1.0}}>
+          <Text style={styles.cardName}>{name}</Text>
+          <Text style={styles.cardDay}>{day}</Text>
+          <Text style={styles.cardMonth}>{month}</Text>
+        </LinearGradient> :
+        <BlurView tint={tint || 'default'} intensity={intensity || 85} style={{ height: '100%', width: '65%', left: 0, position: 'relative', borderBottomLeftRadius: 12, borderTopLeftRadius: 12 }}>
+          <Text style={styles.cardName}>{name}</Text>
+          <Text style={styles.cardDay}>{day}</Text>
+          <Text style={styles.cardMonth}>{month}</Text>
+        </BlurView>
+      }
+        <View style={styles.outerCircle}>
+          <View style={styles.innerCircle}>
+            { registered ?
+              <MaterialCommunityIcons style={styles.checkIcon} name="check-all" size={20} color="#512de0" /> :
+              <Entypo style={styles.playIcon} name="controller-play" size={20} color="#4922e3" /> }
           </View>
-        </ImageBackground>
-      </TouchableOpacity>
-    );
-  }
+        </View>
+      </ImageBackground>
+    </TouchableOpacity>
+  );
 }
+
+export default EventCard;
--- src/components/home/profile.js
+++ src/components/home/profile.js
@@ -1,116 +1,114 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { View, Text, Image } from 'react-native';
 import { LinearGradient } from 'expo';
 import StatusIcon from '../icons/statusIcon';
 
-export default class Profile extends Component {
-  render() {
-    const { name, status, hours, ops, certs } = this.props;
-    return (
-      <View style={styles.userInfo}>
-        <View style={styles.avatar}>
-          <Image
-            source={{ uri: 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/0f/0f15e3ef2fa010110bb70fc833fc9772c3deaf0b_full.jpg' }}
-            style={{ flex: 1, height: 65, width: 65, borderRadius: 32.5 }}
-          />
+const Profile = ({ name, status, hours, ops, certs }) => {
+  const styles = {
+    divider: {
+      height: 0.5,
+      width: '100%',
+      backgroundColor: '#ccc',
+      marginTop: 27.5,
+      marginBottom: 30
+    },
+    statsBar: {
+      flex: 1,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      backgroundColor: 'transparent'
+    },
+    stat: {
+      flex: 1,
+      backgroundColor: 'transparent'
+    },
+    statValue: {
+      fontFamily: 'roboto-light',
+      fontSize: 32,
+      letterSpacing: 0.025,
+      backgroundColor: 'transparent',
+      color: '#32383c',
+      marginBottom: 2.5,
+      right: 1
+    },
+    statName: {
+      fontFamily: 'rubik',
+      fontSize: 11,
+      letterSpacing: 0.45,
+      backgroundColor: 'transparent',
+      color: '#747b8a',
+      left: 2
+    },
+    avatar: {
+      height: 65,
+      width: 65,
+      borderRadius: 32.5,
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      shadowColor: '#000',
+      shadowOffset: { width: 0, height: 1 },
+      shadowOpacity: 0.15,
+      shadowRadius: 14,
+      elevation: 1
+    },
+    userInfo: {
+      marginLeft: 32.5,
+      marginTop: 35
+    },
+    name: {
+      fontFamily: 'syncopate',
+      fontSize: 26,
+      letterSpacing: 1,
+      backgroundColor: 'transparent',
+      color: '#252525',
+      marginTop: 35
+    },
+    status: {
+      flex: 1,
+      flexDirection: 'row',
+      alignItems: 'center',
+      right: 3
+    },
+    statusText: {
+      fontFamily: 'rubik',
+      fontSize: 12,
+      letterSpacing: 0.25,
+      backgroundColor: 'transparent',
+      color: '#747b8a',
+      marginLeft: 5
+    }
+  };
+  return (
+    <View style={styles.userInfo}>
+      <View style={styles.avatar}>
+        <Image
+          source={{ uri: 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/0f/0f15e3ef2fa010110bb70fc833fc9772c3deaf0b_full.jpg' }}
+          style={{ flex: 1, height: 65, width: 65, borderRadius: 32.5 }}
+        />
+      </View>
+      <Text style={styles.name}>{name}</Text>
+      <View style={styles.status}>
+        <StatusIcon />
+        <Text style={styles.statusText}>{status}</Text>
+      </View>
+      <LinearGradient style={styles.divider} start={{x: 0.0, y: 0.25}} end={{x: 0.5, y: 1.0}} locations={[0.3, 1]} colors={['#ccd5da', '#fff']} />
+      <View style={styles.statsBar}>
+        <View style={styles.stat}>
+          <Text style={styles.statValue}>{hours}</Text>
+          <Text style={styles.statName}>Hours</Text>
         </View>
-        <Text style={styles.name}>{name}</Text>
-        <View style={styles.status}>
-          <StatusIcon />
-          <Text style={styles.statusText}>{status}</Text>
+        <View style={styles.stat}>
+          <Text style={styles.statValue}>{ops}</Text>
+          <Text style={styles.statName}>Ops</Text>
         </View>
-        <LinearGradient style={styles.divider} start={{x: 0.0, y: 0.25}} end={{x: 0.5, y: 1.0}} locations={[0.3, 1]} colors={['#ccd5da', '#fff']} />
-        <View style={styles.statsBar}>
-          <View style={styles.stat}>
-            <Text style={styles.statValue}>{hours}</Text>
-            <Text style={styles.statName}>Hours</Text>
-          </View>
-          <View style={styles.stat}>
-            <Text style={styles.statValue}>{ops}</Text>
-            <Text style={styles.statName}>Ops</Text>
-          </View>
-          <View style={styles.stat}>
-            <Text style={styles.statValue}>{certs}</Text>
-            <Text style={styles.statName}>Certifications</Text>
-          </View>
+        <View style={styles.stat}>
+          <Text style={styles.statValue}>{certs}</Text>
+          <Text style={styles.statName}>Certifications</Text>
         </View>
       </View>
-    );
-  }
+    </View>
+  );
 }
 
-const styles = {
-  divider: {
-    height: 0.5,
-    width: '100%',
-    backgroundColor: '#ccc',
-    marginTop: 27.5,
-    marginBottom: 30
-  },
-  statsBar: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    backgroundColor: 'transparent'
-  },
-  stat: {
-    flex: 1,
-    backgroundColor: 'transparent'
-  },
-  statValue: {
-    fontFamily: 'roboto-light',
-    fontSize: 32,
-    letterSpacing: 0.025,
-    backgroundColor: 'transparent',
-    color: '#32383c',
-    marginBottom: 2.5,
-    right: 1
-  },
-  statName: {
-    fontFamily: 'rubik',
-    fontSize: 11,
-    letterSpacing: 0.45,
-    backgroundColor: 'transparent',
-    color: '#747b8a',
-    left: 2
-  },
-  avatar: {
-    height: 65,
-    width: 65,
-    borderRadius: 32.5,
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 1 },
-    shadowOpacity: 0.15,
-    shadowRadius: 14,
-    elevation: 1
-  },
-  userInfo: {
-    marginLeft: 32.5,
-    marginTop: 35
-  },
-  name: {
-    fontFamily: 'syncopate',
-    fontSize: 26,
-    letterSpacing: 1,
-    backgroundColor: 'transparent',
-    color: '#252525',
-    marginTop: 35
-  },
-  status: {
-    flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    right: 3
-  },
-  statusText: {
-    fontFamily: 'rubik',
-    fontSize: 12,
-    letterSpacing: 0.25,
-    backgroundColor: 'transparent',
-    color: '#747b8a',
-    marginLeft: 5
-  }
-};
+export default Profile;
