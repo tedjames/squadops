ed1305786a21da54e4334869dd5fe9050968c4fa
Ted Werbel <werbel.ted@gmail.com>
2017-12-26 17:34:43 -0500

    unified section component to be reused with all event types and servers

--- src/components/home/section.js
+++ src/components/home/section.js
@@ -1,23 +1,63 @@
 import React from 'react';
-import { ScrollView, View, Text, Animated, Dimensions } from 'react-native';
+import { View, Text, Animated, Dimensions, FlatList } from 'react-native';
+import EventCard from './eventCard';
+import ServerCard from './serverCard';
+import ShortcutCard from './shortcutCard';
 
+// Navigation helper HOC
+import NavActions from '../../hocs/eventNavActions';
+
+// Constants
 const screenWidth = Dimensions.get('window').width;
 const cardWidth = screenWidth / 1.25;
 
-const Section = ({ title, style, children, snapToInterval, sectionPosition }) => {
+// Makes FlatList compatible w/ Animated
+const AnimatedFlatList = Animated.createAnimatedComponent(FlatList);
+
+// Rendering functions for FlatList items
+const renderShortcut = ({ iconType, name, size, label, top }, navigate) => {
+  return <ShortcutCard iconType={iconType} name={name} size={size} label={label} top={top} />;
+}
+const renderOperation = ({ name, day, month, image, registered }, navigate) => {
+  const Operation = NavActions(EventCard, navigate, 'Operation');
+  return <Operation name={name} day={day} month={month} image={image} registered={registered} />;
+}
+const renderServer = ({ playerCount, map, name, day, month, intensity, image, gradient, tint }, navigate) => {
+  const Server = NavActions(ServerCard, navigate, 'Server');
+  return <Server playerCount={playerCount} map={map} name={name} day={day} month={month} intensity={intensity} image={image} gradient={gradient} tint={tint} />;
+}
+
+// Rendering helper to determine what to render based on item type ('shortcuts', 'servers', 'operations', or 'training')
+const renderItem = (type, item, navigate) => {
+  switch (type) {
+    case 'shortcuts':
+      return renderShortcut(item, navigate);
+    case 'servers':
+      return renderServer(item, navigate);
+    case 'operations':
+      return renderOperation(item, navigate);
+    case 'training':
+      return renderOperation(item, navigate);
+    default:
+      console.error('Event type not defined. Please set type prop to "shortcuts", "servers", "operations", or "training". ');
+      break;
+  }
+}
+
+const EventSection = ({ title, type, sectionPosition, data, navigate, snapToInterval, style }) => {
   return (
     <View style={{ flex: 1 }}>
       <Text style={styles.sectionName}>{title}</Text>
-      <Animated.ScrollView
+      <AnimatedFlatList
         style={[style || null, { transform: [{ translateY: sectionPosition }] }]}
         contentContainerStyle={{ paddingRight: 15 }}
         showsHorizontalScrollIndicator={false}
         snapToInterval={snapToInterval || cardWidth}
         decelerationRate={'fast'}
         horizontal
-      >
-        {children}
-      </Animated.ScrollView>
+        data={data}
+        renderItem={({ item }) => renderItem(type, item, navigate)}
+      />
     </View>
   );
 };
@@ -34,4 +74,4 @@ const styles = {
   },
 };
 
-export default Section;
+export default EventSection;
