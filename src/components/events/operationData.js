c669bb590fa4d4afdad30553244185e895118ec1
Ted Werbel <werbel.ted@gmail.com>
2017-12-29 15:31:11 -0500

    moved operation data to standalone file (to be later replaced w/ an API)

---
+++ src/components/events/operationData.js
@@ -0,0 +1,357 @@
+const roster = {
+  commands: [
+    {
+      key: '0',
+      name: 'Jack_Reynolds'
+    },
+    {
+      key: '1',
+      name: 'BestPony'
+    },
+  ],
+  squadLeaders: [
+    {
+      key: '0',
+      name: 'Naccho24'
+    },
+    {
+      key: '1',
+      name: '[Ops] SilentDeath'
+    },
+    {
+      key: '2',
+      name: '[Ops] EvanSMA'
+    },
+    {
+      key: '3',
+      name: 'ShadowedRitual'
+    },
+    {
+      key: '4',
+      name: '[Ops] CrustyTheSailor'
+    },
+    {
+      key: '5',
+      name: '[Ops] NastyNate'
+    },
+    {
+      key: '6',
+      name: 'Kennet'
+    },
+    {
+      key: '7',
+      name: '[Ops] Satan'
+    },
+  ],
+  admins: [
+    {
+      key: '0',
+      name: '[Ops] EvanSMA'
+    },
+    {
+      key: '1',
+      name: '[Ops] Burns'
+    }
+  ],
+  commentators: [
+    {
+      key: '0',
+      name: 'Karmakut'
+    },
+    {
+      key: '1',
+      name: 'TheMuffBandit'
+    },
+  ],
+  specialGuests: [
+    {
+      key: '0',
+      name: 'Diplexheated'
+    },
+  ],
+  confirmed: [
+    {
+      key: '0',
+      name: 'guy wit a goat'
+    },
+    {
+      key: '1',
+      name: '[Ops] TurtleguyFive'
+    },
+    {
+      key: '2',
+      name: '[Ops] Triton'
+    },
+    {
+      key: '3',
+      name: 'Kennet'
+    },
+    {
+      key: '4',
+      name: 'TheMuffBandit'
+    },
+    {
+      key: '5',
+      name: '[Ops] Bellator'
+    },
+    {
+      key: '6',
+      name: '[Ops] SMPureParadise'
+    },
+    {
+      key: '7',
+      name: 'Dorf'
+    },
+    {
+      key: '8',
+      name: 'Jenkins'
+    },
+    {
+      key: '9',
+      name: 'Sightless'
+    },
+    {
+      key: '10',
+      name: 'Miyamoto'
+    },
+    {
+      key: '11',
+      name: '[Ops] Benbot16'
+    },
+    {
+      key: '12',
+      name: 'Jaster'
+    },
+    {
+      key: '13',
+      name: '[Ops] HanVonSolo'
+    },
+    {
+      key: '14',
+      name: 'Supra'
+    },
+    {
+      key: '15',
+      name: 'PAINKILLER'
+    },
+    {
+      key: '16',
+      name: 'Lani'
+    },
+    {
+      key: '17',
+      name: 'PitViper'
+    },
+    {
+      key: '18',
+      name: 'stormfront489'
+    },
+    {
+      key: '19',
+      name: 'DashBee'
+    },
+    {
+      key: '20',
+      name: 'fx1000'
+    },
+    {
+      key: '21',
+      name: 'Drunique'
+    },
+    {
+      key: '22',
+      name: 'PandaScopeXL'
+    },
+    {
+      key: '23',
+      name: 'laki_proo'
+    },
+  ],
+  reserves: [
+    {
+      key: '0',
+      name: 'guy wit a goat'
+    },
+    {
+      key: '1',
+      name: '[Ops] TurtleguyFive'
+    },
+    {
+      key: '2',
+      name: '[Ops] Triton'
+    },
+    {
+      key: '3',
+      name: 'Kennet'
+    },
+    {
+      key: '4',
+      name: 'TheMuffBandit'
+    },
+    {
+      key: '5',
+      name: '[Ops] Bellator'
+    },
+    {
+      key: '6',
+      name: '[Ops] SMPureParadise'
+    },
+    {
+      key: '7',
+      name: 'Dorf'
+    },
+    {
+      key: '8',
+      name: 'Jenkins'
+    },
+    {
+      key: '9',
+      name: 'Sightless'
+    },
+    {
+      key: '10',
+      name: 'Miyamoto'
+    },
+    {
+      key: '11',
+      name: '[Ops] Benbot16'
+    },
+    {
+      key: '12',
+      name: 'Jaster'
+    },
+    {
+      key: '13',
+      name: '[Ops] HanVonSolo'
+    },
+    {
+      key: '14',
+      name: 'Supra'
+    },
+    {
+      key: '15',
+      name: 'PAINKILLER'
+    },
+    {
+      key: '16',
+      name: 'Lani'
+    },
+    {
+      key: '17',
+      name: 'PitViper'
+    },
+    {
+      key: '18',
+      name: 'stormfront489'
+    },
+    {
+      key: '19',
+      name: 'DashBee'
+    },
+    {
+      key: '20',
+      name: 'fx1000'
+    },
+    {
+      key: '21',
+      name: 'Drunique'
+    },
+    {
+      key: '22',
+      name: 'PandaScopeXL'
+    },
+    {
+      key: '23',
+      name: 'laki_proo'
+    },
+    {
+      key: '24',
+      name: 'laki_proo2'
+    },
+  ]
+}
+const operationDetails = {
+  description: 'US command has identified an insurgent stronghold within a village, just a few kilometers from our forward operating base in the suburbs of Al Basrah. Our objective is to eliminate all hostiles and protect the FOB.',
+  victoryConditions: [
+    {
+      key: '0',
+      value: 'Destroy insurgent FOB'
+    },
+    {
+      key: '1',
+      value: 'Eliminate all insurgent forces in the area'
+    }
+  ],
+  infantryAssets: [
+    {
+      key: '0',
+      value: 'M4'
+    },
+    {
+      key: '1',
+      value: 'M249 SAW'
+    },
+    {
+      key: '2',
+      value: 'M203'
+    },
+    {
+      key: '3',
+      value: 'M72 LAW'
+    }
+  ],
+  supportAssets: [
+    {
+      key: '0',
+      value: 'HMMWV'
+    },
+    {
+      key: '1',
+      value: 'Logistic truck'
+    }
+  ],
+  logisticAssets: [
+    {
+      key: '0',
+      value: 'COP'
+    },
+    {
+      key: '1',
+      value: 'Possible heavy weapons'
+    }
+  ],
+  mobilityRestrictions: [
+    {
+      key: '0',
+      value: 'Vehicles must use bridges'
+    }
+  ],
+  beforeLive: [
+    {
+      key: '0',
+      value: 'Decide on COP location'
+    }
+  ],
+  specialtyRules: [
+    {
+      key: '0',
+      value: 'Destroy insurgent FOB'
+    },
+    {
+      key: '1',
+      value: 'Eliminate all insurgent forces in the area'
+    }
+  ],
+  intel: [
+    {
+      key: '0',
+      value: 'Destroy insurgent FOB'
+    },
+    {
+      key: '1',
+      value: 'Eliminate all insurgent forces in the area'
+    }
+  ]
+}
+
+export { roster, operationDetails };
